bcdd9b00ba60651d87ef27afcfc91ec6cc61239f
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-03-18 17:19:28 +0800

    init: 初始化 SpringBoot + Java + JPA 后端框架

---
+++ server/src/main/java/com/tomato/bookstore/TomatoBookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.tomato.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoBookstoreApplication.class, args);
+    }
+}
