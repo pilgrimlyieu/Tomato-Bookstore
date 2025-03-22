14048430d0a92695095bc4f325d044cd52dea709
PilgrimLyieu <80107081+pilgrimlyieu@users.noreply.github.com>
2025-03-22 23:29:58 +0800

    Update server/src/main/java/com/tomato/bookstore/constant/HttpStatus.java
    Co-authored-by: fephkol <141289472+fephkol@users.noreply.github.com>

--- server/src/main/java/com/tomato/bookstore/constant/HttpStatus.java
+++ server/src/main/java/com/tomato/bookstore/constant/HttpStatus.java
@@ -6,10 +6,12 @@ package com.tomato.bookstore.constant;
 public enum HttpStatus {
     OK(200, "操作成功"),
     CREATED(201, "创建成功"),
+
     BAD_REQUEST(400, "请求参数错误"),
     UNAUTHORIZED(401, "未授权"),
     FORBIDDEN(403, "禁止访问"),
     NOT_FOUND(404, "资源不存在"),
+
     INTERNAL_SERVER_ERROR(500, "服务器内部错误");
 
     private final int code;
