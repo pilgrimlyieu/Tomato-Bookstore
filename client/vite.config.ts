40c32cd21fc33fcd1592acf0ddb76f4f9a0395e2
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-03-24 19:07:45 +0800

    feat: 更新 TailwindCSS 引入方式，优化 Vite 配置，添加路径别名和自动导入支持

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,37 +1,40 @@
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
-import { defineConfig } from 'vite'
+import path from "node:path";
+import { URL, fileURLToPath } from "node:url";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@stores': path.resolve(__dirname, 'src/stores')
-    }
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      imports: ['vue'],
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/auto-imports.d.ts',
-    }),
-    Components({
-      dirs: ['src/components'],
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/components.d.ts',
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			"@components": path.resolve(__dirname, "src/components"),
+			"@stores": path.resolve(__dirname, "src/stores"),
+			"@utils": path.resolve(__dirname, "src/utils"),
+			"@views": path.resolve(__dirname, "src/views"),
+		},
+	},
+	plugins: [
+		vue(),
+		AutoImport({
+			imports: ["vue", "vue-router"],
+			resolvers: [ElementPlusResolver()],
+			dts: "src/auto-imports.d.ts",
+		}),
+		Components({
+			dirs: ["src/components"],
+			resolvers: [ElementPlusResolver()],
+			dts: "src/components.d.ts",
+		}),
+	],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8080",
+				changeOrigin: true,
+			},
+		},
+	},
+});
