88baee360598dd981bc1791f00989d2c2b506451
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-03-31 15:38:24 +0800

    chore(deps): 更新依赖与 `vite.config.ts`

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,3 @@
-import { URL, fileURLToPath } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
@@ -8,7 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": "/src",
     },
   },
   plugins: [
