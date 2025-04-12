14d0e026484bf2f1e97ee84d3fb56e3b60be901e
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-04-12 16:38:08 +0800

    feat(test): 添加广告模块相关测试常量和创建测试广告的方法

--- server/src/test/java/com/tomato/bookstore/constant/TestConstants.java
+++ server/src/test/java/com/tomato/bookstore/constant/TestConstants.java
@@ -51,4 +51,13 @@ public class TestConstants {
   public static final String TEST_SHIPPING_ADDRESS = "测试地址，测试街道 123 号";
   public static final Integer TEST_ORDER_QUANTITY = 3;
   public static final String TEST_ALIPAY_TRADE_NO = "2023112822001123456789";
+
+  // 广告模块常量
+  public static final String TEST_ADVERTISEMENT_TITLE = "测试广告";
+  public static final String TEST_ADVERTISEMENT_CONTENT = "这是一条测试广告内容";
+  public static final String TEST_ADVERTISEMENT_IMAGE_URL = "https://example.com/test-ad-image.jpg";
+  public static final String TEST_ADVERTISEMENT_PRODUCT_NAME = "广告测试商品";
+
+  // 测试用户名
+  public static final String TEST_CUSTOMER_USERNAME = "customer";
 }
