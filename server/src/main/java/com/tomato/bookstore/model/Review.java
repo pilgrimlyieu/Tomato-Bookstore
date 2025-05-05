a5bf8e0739a6ccc88f67d15ab88777291035fdb9
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-05-05 11:57:53 +0800

    refactor(model): 优化 Review 类，添加产品和用户的关联，提升数据模型的完整性

--- server/src/main/java/com/tomato/bookstore/model/Review.java
+++ server/src/main/java/com/tomato/bookstore/model/Review.java
@@ -2,9 +2,12 @@ package com.tomato.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -24,8 +27,16 @@ public class Review {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", insertable = false, updatable = false)
+  private Product product;
+
   @Column(nullable = false)
   private Long productId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", insertable = false, updatable = false)
+  private User user;
 
   @Column(nullable = false)
   private Long userId;
