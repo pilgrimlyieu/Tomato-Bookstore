09894c2409ee4803c51096a29bdb6f52cc23deb0
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-05-09 21:18:13 +0800

    feat(constants): 添加笔记模块相关常量和路由定义，更新错误码及消息

--- client/src/constants/apiPrefix.ts
+++ client/src/constants/apiPrefix.ts
@@ -4,3 +4,4 @@ export const CART_MODULE = "/cart";
 export const ORDER_MODULE = "/orders";
 export const ADVERTISEMENT_MODULE = "/advertisements";
 export const REVIEW_MODULE = "/reviews";
+export const NOTE_MODULE = "/notes";
